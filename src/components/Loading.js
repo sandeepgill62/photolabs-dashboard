4811da7032f1391d8432d34c45f764cfde29f463
Sandeep Gill <sandeepgill62@gmail.com>
2023-08-28 01:00:05 +0000

    loading class component

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Loading extends Component {
+ 
+  render() {
+    return (
+      <section className="loading">
+        Loading
+      </section>
+    );
+  }
+}
